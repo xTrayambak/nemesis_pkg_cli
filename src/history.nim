60975a8a64fef126bc3657cc877437d42b632334
xTrayambak <xtrayambak@gmail.com>
2023-06-25 12:31:39 +0530

    (fix) actually fixed formatting, for reals

--- src/history.nim
+++ src/history.nim
@@ -1,11 +1,13 @@
 import std/[os, strformat, times]
 
+const nextline = "\n"
+
 proc writeHistory*(currentOpr: string) =
  let histfile = fmt"{getHomeDir()}/.nemesis-pkg_history"
  if fileExists(histfile):
   let file = open(histfile, fmAppend)
   defer: file.close()
-  file.write(fmt"\n{now()} {currentOpr}")
+  file.write(fmt"{nextline}{now()} {currentOpr}")
  else:
   let file = open(histfile, fmWrite)
   defer: file.close()
